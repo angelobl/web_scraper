1033dcf299432521b1e2cacabcc1fc2feaf0d3a6
Angelo Bertello <bertello.angelo@gmail.com>
2019-11-06 13:31:00 -0500

    Termine scraper de Ebay

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const krowdy = express.Router();
 
-const scraper = require('../scraper/scraper')
+const scraperWong = require('../scraper/scraper')
+const scraperEbay = require('../scraper/scraper_ebay')
 
 krowdy.get('/', async (req,res) => {
     const scrap = await scraper();
@@ -9,8 +10,13 @@ krowdy.get('/', async (req,res) => {
   });
 
   krowdy.get('/wong', async (req,res) => {
-    const scrap = await scraper();
-    res.render('scraper/scraper_wong',{scrap:scrap});
+    const scrapWong = await scraperWong();
+    res.render('scraper/scraper_wong',{scrap:scrapWong});
+  });
+
+  krowdy.get('/ebay', async (req,res) => {
+    const scrapEbay = await scraperEbay();
+    res.render('scraper/scraper_ebay',{scrap:scrapEbay});
   });
 
 module.exports = krowdy;
